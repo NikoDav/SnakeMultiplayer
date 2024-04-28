eb6a7e1befa0eadaa34ac90ab75eb6c14447aa4e
NikoDav <165057786+NikoDav@users.noreply.github.com>
2024-04-28 13:00:56 -0400

    AddedPlayerNameSystem

---
+++ Assets/Scripts/PlayerName.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class PlayerName : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+    [SyncVar(hook = nameof(HandleName))] private string _playerName;
+
+
+    private void HandleName(string oldText, string newText)
+    {
+        _text.text = $"Player: {_playerName}";
+    }
+
+    public override void OnStartServer()
+    {
+        _playerName = connectionToClient.connectionId.ToString();
+    }
+}
