e308291d69fd1b00c1be29ca5cccebc19de60bfe
NikoDav <165057786+NikoDav@users.noreply.github.com>
2024-05-04 10:38:02 -0400

    MadeFoodParticlesServerWide

--- Assets/Scripts/Food/Food.cs
+++ Assets/Scripts/Food/Food.cs
@@ -8,17 +8,42 @@ public class Food : NetworkBehaviour
 {
     [SerializeField] GameObject particlePrefab;
     public static event Action ServerOnFoodEaten;
+    private float elapsedTime;
+    private GameObject currentParticle;
+
+    private void Update()
+    {
+        if(currentParticle == null)
+        {
+            return;
+        }
+
+
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= 3)
+        {
+            NetworkServer.Destroy(currentParticle);
+            elapsedTime = 0;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         FindObjectOfType<Snake>().AddTail();
-        GameObject boom = Instantiate
+        currentParticle = Instantiate
             (particlePrefab, transform.position, particlePrefab.transform.rotation);
-        Destroy(boom, 3f);
+        NetworkServer.Spawn(currentParticle);
+        //StartCoroutine(DelayDestroyBoom());
         Destroy(gameObject);
         FindObjectOfType<FoodSpawner>().SpawnFood();
         ServerOnFoodEaten?.Invoke();
         NetworkServer.Destroy(gameObject);
     }
+
+   /* private IEnumerator DelayDestroyBoom()
+    {
+        yield return new WaitForSeconds(3);
+        NetworkServer.Destroy(currentParticle);
+    }*/
 }
